a0eaf5047e1cff7ba91e295ffa7804d61f440ee8
Jonatan Männchen <jonatan@maennchen.ch>
2018-12-30 13:30:48 +0100

    Setup Testing

---
+++ priv/test/example_apps/vulnerable/mix.exs
@@ -0,0 +1,27 @@
+defmodule Vulnerable.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :vulnerable,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:xain, "0.6.1"}
+    ]
+  end
+end
